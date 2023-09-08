8d9bd3d006710a21a55ff9cdd90a2a1f14521134
Rishi <rishiraj1096@gmail.com>
2023-09-08 18:50:24 +0530

    initial-commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 app.use(cookieParser);
 app.use(cors({
     credentials:true,
-    methods:["GET","POST","PUT","DELETE"],
+    methods:["GET","POST","PUT","DELETE"]
 }))
 
 //Importing & using Routes
